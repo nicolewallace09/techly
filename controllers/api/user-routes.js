7ee39ebfa5c6dac421ae41e3646832fa6c0d9be2
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-18 20:55:19 -0700

    added user delete/edit, added helper function

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -122,6 +122,48 @@ router.post('/logout', (req, res) => {
     }
 });
 
+// PUT /api/users/1 - similar to UPDATE 
+router.put('/:id', /*withAuth,*/ (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err);
+    });
+
+});
+
+// DELETE /api/users/1
+router.delete('/:id', /*withAuth,*/ (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
 
 
