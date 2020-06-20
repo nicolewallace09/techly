e8f15b931b23b94b1ad326b95e4415d4438baaf2
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-20 16:34:03 -0700

    still working on homepage

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -3,14 +3,14 @@ async function upvoteClickHandler(event) {
     event.preventDefault();
 
   // taking id from http://localhost:3001/post/1
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-  ];
+//   const id = window.location.toString().split('/')[
+//     window.location.toString().split('/').length - 1
+//   ];
 
   const response = await fetch('/api/posts/upvote', {
     method: 'PUT',
     body: JSON.stringify({
-      post_id: id
+      post_id: post.post_id
     }),
     headers: {
       'Content-Type': 'application/json'
