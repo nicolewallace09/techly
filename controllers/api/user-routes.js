7a09f9ea0bb9bb415f9df7ce04e2675466cbd3a3
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-19 18:38:29 -0700

    added latest commits for pull

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
 // GET /api/users
 // http://localhost:3001/api/users
