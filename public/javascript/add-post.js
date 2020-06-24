0ba236146c0dba2fe1fb6de2b97f2a02514ddf5f
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-23 18:47:27 -0700

    added some seeds, saving before pulling from master

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -8,7 +8,8 @@ async function newFormHandler(event) {
         method: 'POST',
         body: JSON.stringify({
         // title,
-        post_text
+        post_text,
+        img
         }),
         headers: {
         'Content-Type': 'application/json'
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -10,7 +10,29 @@ const postData = [
         // "title": "Life Update",
         "post_text": "Does anyone know how to implement infinite scrolling?!",
         "user_id": 2
+    },
+    {
+        // "title": "Life Update",
+        "post_text": "Handlebars is killing me.",
+        "user_id": 2
+    }, 
+    {
+        // "title": "Life Update",
+        "post_text": "Hey guys, here's a cool npm package for uploading images -  https://www.npmjs.com/package/multer",
+        "user_id": 3
     }
+    ,  
+    {
+        // "title": "Life Update",
+        "post_text": "What are some good npm resources for a project?",
+        "user_id": 3
+    },
+    {
+        // "title": "Life Update",
+        "post_text": "Just finished working on a search function for my project!",
+        "user_id": 1
+    }
+
 ]
 
 const seedPosts = () => Post.bulkCreate(postData);
