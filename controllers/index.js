585f2fc414180313ebe597616432095329d868a1
ktrnthsnr <asakthisner@hotmail.com>
2020-06-24 04:36:29 -0700

    updated search dependencies

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,13 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const profileRoutes = require('./profile-routes.js');
 const profileCardRoutes = require('./profile-card-routes')
+const searchRoutes = require('./search-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/profile', profileRoutes);
-router.use('/profile', profileCardRoutes);
+router.use('/profile-card-routes', profileCardRoutes);
+router.use('/search', searchRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
