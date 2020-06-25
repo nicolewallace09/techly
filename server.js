835605d34867a99a3fb46f4e2a2c0b6ac672061c
nicolewallace09 <nicole.elisaw@gmail.com>
2020-06-25 13:13:52 -0700

    seeds

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'project2 super secret',
-  cookie: { maxAge: 60000},
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -43,26 +43,26 @@ app.use(routes);
 // passport middleware
 //var app = express();
 //app.use(require('serve-static')(__dirname + '/../../public'));
-const passport = require('passport');
-const flash = require('express-flash')
-var cookieParser = require('cookie-parser')
+// const passport = require('passport');
+// const flash = require('express-flash')
+// var cookieParser = require('cookie-parser')
 
 
 
-const initializePassport = require('/passport-config');
+// const initializePassport = require('/passport-config');
 
-initializePassport(
-  passport, 
-  email => users.find(user => user.email === email)
-);
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
+// initializePassport(
+//   passport, 
+//   email => users.find(user => user.email === email)
+// );
+// app.use(require('cookie-parser')());
+// app.use(require('body-parser').urlencoded({ extended: true }));
 
 
-app.use(flash())
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(flash())
+// app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
 // turn on connection to db and server
