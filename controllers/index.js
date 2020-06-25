9ac2ed45385902deba4de1cd756276d4cad0503c
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-06-24 22:08:07 -0700

    added search route in index to limit conflicts with merge

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,14 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const profileRoutes = require('./profile-routes.js');
 const singleProfileRoutes = require('./single-profile-routes')
+const searchRoutes = require('./search-routes.js');
 //const profileCardRoutes = require('./profile-card-routes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/profile', profileRoutes);
-router.use('/single-profile', singleProfileRoutes )
+router.use('/single-profile', singleProfileRoutes)
+router.use('/search', searchRoutes);
 //router.use('/profile', profileCardRoutes);
 
 router.use((req, res) => {
